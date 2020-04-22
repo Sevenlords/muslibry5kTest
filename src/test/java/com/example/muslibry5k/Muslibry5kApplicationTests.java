a063a8d64704e67bb2550b9cce4d5cdbca667c30
Vova <volodymyr.bunin@gmail.com>
2020-04-22 20:25:36 +0200

    Initial commit

---
+++ src/test/java/com/example/muslibry5k/Muslibry5kApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muslibry5k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Muslibry5kApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
